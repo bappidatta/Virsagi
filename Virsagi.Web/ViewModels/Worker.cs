5a42a313b8f2496812b0f0c5b3643676a353617b
Bappi Datta <bappigeek@gmail.com>
2018-12-02 22:17:15 +0600

    add images

---
+++ Virsagi.Web/ViewModels/Worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Virsagi.Web.ViewModels
+{
+    public class Worker
+    {
+        public int WorkerID { get; set; }
+        public string Name { get; set; }
+    }
+}
