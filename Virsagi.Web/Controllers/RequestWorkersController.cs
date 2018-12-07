1f523a2898b8dd00daa48295d2535de8582aa5ac
Bappi Datta <bappigeek@gmail.com>
2018-12-07 15:12:11 +0600

    worker request save

--- Virsagi.Web/Controllers/RequestWorkersController.cs
+++ Virsagi.Web/Controllers/RequestWorkersController.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Virsagi.Web.Models;
+using Virsagi.Web.ViewModels;
 
 namespace Virsagi.Web.Controllers
 {
     public class RequestWorkersController : Controller
     {
-        // GET: RequestWorkers
+        private VirsagiContext db;
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(WorkerRequestViewModel vm)
+        {
+            if(ModelState.IsValid)
+            {
+                db = new VirsagiContext();
+
+                var workerRequest = new WorkerRequest
+                {
+                    RequestType = vm.RequestType,
+                    ContactPerson = vm.ContactPerson,
+                    ContactNumber = vm.ContactNumber,
+                    CompanyUENNumber = vm.CompanyUENNumber,
+                    CompanyName = vm.CompanyName,
+                    Email = vm.Email,
+                    Details = vm.Details,
+                    SpecialRequest = vm.SpecialRequest,
+                    CreatedDate = DateTime.Now
+                };
+
+                db.WorkerRequests.Add(workerRequest);
+                db.SaveChanges();
+
+                TempData["isSuccess"] = 1;
+
+                return RedirectToAction("Index");
+            }
+
+            TempData["isSuccess"] = 0;
+            return View("Index", vm);
         }
     }
 }
---
+++ Virsagi.Web/ViewModels/WorkerRequestViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Virsagi.Web.ViewModels
+{
+    public class WorkerRequestViewModel
+    {
+        public int WorkerRequestID { get; set; }
+        public int RequestType { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string ContactPerson { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string ContactNumber { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string CompanyUENNumber { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string CompanyName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Email { get; set; }
+
+        public string Details { get; set; }
+        public string SpecialRequest { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
