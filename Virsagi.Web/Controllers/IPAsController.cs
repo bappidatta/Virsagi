d016f571eea2488397dcd0f8daab0f5215791d70
Bappi Datta <bappigeek@gmail.com>
2019-09-30 16:48:23 +0600

    show ipa listing

---
+++ Virsagi.Web/Controllers/IPAsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Virsagi.Web.Models;
+using Virsagi.Web.ViewModels;
+
+namespace Virsagi.Web.Controllers
+{
+    public class IPAsController : Controller
+    {
+        private VirsagiContext db;
+
+        public ActionResult Index(DateTime? fromDate, DateTime? toDate)
+        {
+            db = new VirsagiContext();
+
+            var query = (from s in db.IPAs
+                         select new IPAViewModel
+                         {
+                             IPAID = s.IPAID,
+                             PassportNo = s.PassportNo,
+                             WorkerName = s.WorkerName,
+                             Employer = s.Employer,
+                             WorkPermitNo = s.WorkPermitNo,
+                             ReferenceNo = s.ReferenceNo,
+                             IssuanceDate = s.IssuanceDate
+                         }).AsQueryable();
+
+            if (fromDate != null && toDate != null)
+            {
+                query = query.Where(x => x.IssuanceDate >= fromDate && x.IssuanceDate <= toDate);
+            }
+
+            var data = query.AsEnumerable();
+
+            return View(data);
+        }
+    }
+}
